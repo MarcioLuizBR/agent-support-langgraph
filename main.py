d3064a8a4c2cbd10556cc7e75da70c77d722336d
Marcio Luiz dos Santos Pereira <133532252+MarcioLuizBR@users.noreply.github.com>
2026-03-18 11:02:44 -0300

    Add initial LLM integration and basic response test

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
+import os
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+
+
+load_dotenv()
+
+api_key = os.getenv("OPENAI_API_KEY")
+
+if not api_key:
+    raise ValueError("A chave de API do OpenAI não foi encontrada. Por favor, defina a variável de ambiente OPENAI_API_KEY.")
+
+llm = ChatOpenAI(
+    model="gpt-4o-mini", 
+    temperature=0, 
+    api_key=api_key
+    )
+
 def main():
-    print("Hello from agent-support-langgraph!")
+    resposta = llm.invoke("Explique em uma frase o que é um agente de IA.")
+    print("\nResposta do modelo:\n")
+    print(resposta.content)
 
 
 if __name__ == "__main__":
